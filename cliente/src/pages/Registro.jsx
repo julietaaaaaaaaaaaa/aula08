8919348a810e43832fb7943d1fb0ba099f933320
Julietaa <julia.souza3@estudante.ifms.edu.br>
2024-11-25 14:37:04 -0400

    eit

--- cliente/src/pages/Registro.jsx
+++ cliente/src/pages/Registro.jsx
@@ -1,9 +1,41 @@
 import { useState } from "react";
 
 export default function Registrar() {
+const[nome, setNome] = useState([]);
 
+const[email, setEmail] = useState([]);
 
-  return (
-        
+const registrar = async(event)=> {
+  event.preventDefault();
+
+  try{
+    await fetch('http://localhost:3000/usuarios',{
+      method: 'POST',
+      headers:{'Content-Type':'application/json'},
+      body: JSON.stringify({
+        nome: nome,
+        email: email
+      })
+    });
+  }catch{
+    alert("ocorreu um erro na aplicação");
+  }
+}  
+
+return (
+     <main>
+<form action="">
+<input
+type="text"
+value={nome}
+onChange={(event)=>setNome(event.target.value)}/>
+<input
+type="email"
+value={email}
+onChange={(event)=>setEmail(event.target.value)}/>
+
+</form>
+
+     </main>   
   );
 }
